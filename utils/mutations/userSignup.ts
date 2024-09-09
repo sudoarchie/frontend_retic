1667441c15ef790a86f520a897ecfb008f5b3b74
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-09-10 01:31:34 +0530

    ✨ New feature new

---
+++ utils/mutations/userSignup.ts
@@ -0,0 +1,27 @@
+import api from "@/app/api";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+const UserSignup = () => {
+  const mutate = useMutation({
+    mutationKey: ["/addtoForm"],
+    mutationFn: async (data: any) => {
+      const res = await api.post("/userAuth/signup", data);
+      return res.data;
+    },
+    onMutate: () => {
+      toast.loading("Waiting...");
+    },
+    onSuccess: () => {
+      toast.dismiss();
+      toast.success("Form filled successfully");
+    },
+    onError: (err) => {
+      toast.dismiss();
+      toast.error(err.message);
+    },
+  });
+  return mutate;
+};
+
+export default UserSignup;
